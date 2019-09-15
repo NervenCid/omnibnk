af0fcfcd66d3b36e27c2c404010cef706f90533d
nervencid <nervencid@gmail.com>
2019-09-15 16:16:25 -0500

    Registro 2

--- users/views.py
+++ users/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+#Importamos un modulo que facilita la creacion de usuarios
+from django.contrib.auth.forms import UserCreationForm
+
+
 #Creamos las vistas para usuario
 
 #Registro
 def register(request):
-    
+    #Creamos el formulario de registro
+    form = UserCreationForm()
+    #Renderizamos en una vista
+    return render(request, 'users/register.html', {'form': form})
